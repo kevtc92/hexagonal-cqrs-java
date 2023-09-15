4bdb6812ce57d50870055d7192e310905fa3150d
Kevin Trejo <kevtc92@gmail.com>
2023-09-15 14:25:14 -0600

    Initial commit

---
+++ src/main/java/com/ktcdev/hexagonalcqrsjava/HexagonalCqrsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ktcdev.hexagonalcqrsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalCqrsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalCqrsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktcdev/hexagonalcqrsjava/HexagonalCqrsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktcdev.hexagonalcqrsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalCqrsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
